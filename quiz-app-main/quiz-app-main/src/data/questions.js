6438957adf9393864cf08379954ebc01732a07e9
amandinha1234 <133913362+amandinha1234@users.noreply.github.com>
2023-10-26 20:17:02 -0300

    Add files via upload

---
+++ quiz-app-main/quiz-app-main/src/data/questions.js
@@ -0,0 +1,75 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: " Quais são os principais desafios enfrentados pelos povos indígenas atualmente?",
+      answer: "Todas as alternativas estão corretas",
+      options: [
+        "Falta de terras demarcadas e invasões de seus territórios",
+        "Ausência de políticas públicas e discriminação social",
+        "Dificuldade de acesso à educação e saúde de qualidade",
+        "Todas as alternativas estão corretas",
+      ],
+    },
+    {
+      numb: 2,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: " Quais são as principais formas de subsistência dos povos indígenas?",
+      answer: "Agricultura e caça",
+      options: [
+        "Pecuária e pesca",
+        "Agricultura e caça",
+        "Comércio e indústria",
+        " Mineração e exploração florestal",
+      ],
+    },
+    {
+      numb: 3,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Quem é responsável pela proteção dos direitos dos povos indígenas no Brasil?",
+      answer: "Governo Federal",
+      options: [
+        "Organização das Nações Unidas(ONU)",
+        "Nações indígenas organizadas",
+        "Governo Federal",
+        "Sociedade civil em geral",
+      ],
+    },
+    {
+      numb: 4,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Qual é a relação dos povos indígenas com o meio ambiente?",
+      answer: "Uso sustentável dos recursos e respeito à natureza",
+      options: [
+        "Exploração desenfreada dos recursos naturais",
+        "Indiferença em relação à preservação ambiental",
+        "Uso sustentável dos recursos e respeito à natureza",
+        "Despreocupação com a conservação dos ecossistemas",
+      ],
+    },
+    {
+      numb: 5,
+      category: "General Knowledge",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Qual era a principal atividade econômica dos povos indígenas do Brasil antes da chegada dos colonizadores europeus?",
+      answer: "Agricultura",
+      options: [
+        "Agricultura",
+        "Pesca",
+        "Criação de gado",
+        "Mineração",
+      ],
+    },
+    
+  ];
+  
